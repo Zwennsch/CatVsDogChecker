627f67e3537f3d9e6afbbea40b5f06872248d545
Sven Schroeder <schuen@gmx.de>
2021-01-20 17:57:10 +0100

    working on home screen

--- lib/home.dart
+++ lib/home.dart
@@ -8,8 +8,41 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Sven Schröder",
+              style: TextStyle(color: Colors.redAccent, fontSize: 20.0),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              "assets/dog.png",
+              scale: 0.1,
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Cat vs Dog Analyzer",
+              style: TextStyle(
+                  color: Colors.redAccent,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 30),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
