dcf35e5c9971fd3679958bf4327d84a63e7afc7c
Sven Schroeder <schuen@gmx.de>
2021-01-22 08:25:29 +0100

    updated home screen

--- lib/home.dart
+++ lib/home.dart
@@ -6,6 +6,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool _loading = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,20 +27,35 @@ class _HomeState extends State<Home> {
             SizedBox(
               height: 20,
             ),
-            Image.asset(
-              "assets/dog.png",
-              scale: 0.1,
-            ),
-            SizedBox(
-              height: 50,
-            ),
             Text(
               "Cat vs Dog Analyzer",
               style: TextStyle(
                   color: Colors.redAccent,
                   fontWeight: FontWeight.w500,
                   fontSize: 30),
-            )
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Center(
+              child: (_loading
+                  ? Container(
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            "assets/dog.png",
+                            scale: 2.5,
+                          ),
+                          SizedBox(
+                            height: 50,
+                          )
+                        ],
+                      ),
+                      width: 350,
+                    )
+                  : Container()),
+            ),
+            Container(width: MediaQuery.of(context).size.width, child: Column(),)
           ],
         ),
       ),
