bbe5f2fc0d2615485a90817875ad2a17a42d346b
Sven Schroeder <schuen@gmx.de>
2021-02-05 15:09:33 +0100

    finished project but could not download pics on device to test

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:tflite/tflite.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -7,6 +12,69 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   bool _loading = true;
+  File _image;
+  List _output;
+  final picker = ImagePicker();
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel().then((value){
+      setState(() {
+
+      });
+    });
+  }
+
+  detectImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+        path: image.path,
+        numResults: 2,
+        threshold: 0.6,
+        imageMean: 127.5,
+        imageStd: 127.5);
+
+    setState(() {
+      _output = output;
+      _loading = false;
+    });
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+        model: "assets/model_unquant.tflite", labels: "assets/labels.txt");
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  pickImage() async {
+    var image = await picker.getImage(
+      source: ImageSource.camera,
+    );
+    if (image == null) return null;
+
+    setState(() {
+      _image = File(image.path);
+    });
+    detectImage(_image);
+  }
+
+  pickGaleryImage() async {
+    var image = await picker.getImage(
+      source: ImageSource.gallery,
+    );
+    if (image == null) return null;
+
+    setState(() {
+      _image = File(image.path);
+    });
+    detectImage(_image);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,14 +121,36 @@ class _HomeState extends State<Home> {
                       ),
                       width: 350,
                     )
-                  : Container()),
+                  : Container(
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 250,
+                            child: Image.file(_image),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          _output != null
+                              ? Text(
+                                  "${_output[0]["label"]}",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 50),
+                                )
+                              : Container(), 
+                              SizedBox(height: 10,)
+                        ],
+                      ),
+                    )),
             ),
             Container(
               width: MediaQuery.of(context).size.width,
               child: Column(
                 children: [
                   GestureDetector(
-                    onTap: () {},
+                    onTap: () {
+                      pickImage();
+                    },
                     child: Container(
                       width: MediaQuery.of(context).size.width - 250,
                       alignment: Alignment.center,
@@ -84,7 +174,9 @@ class _HomeState extends State<Home> {
                     height: 15,
                   ),
                   GestureDetector(
-                    onTap: () {},
+                    onTap: () {
+                      pickGaleryImage();
+                    },
                     child: Container(
                       width: MediaQuery.of(context).size.width - 250,
                       alignment: Alignment.center,
